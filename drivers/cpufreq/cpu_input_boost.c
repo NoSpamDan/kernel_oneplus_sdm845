c8c9754652c280c3026121652ae9af732a70e91e
kdrag0n <dragon@khronodragon.com>
2018-12-07 22:38:29 -0800

    cpu_input_boost: expose API for boosting CPUs on wake
    Also, remove unused WAKE_BOOST bit.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    [@0ctobot: Squash kdrag0n/proton_bluecross@d090e52 with kdrag0n/proton_bluecross@ee77bdf]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -27,8 +27,7 @@ module_param(input_boost_duration, short, 0644);
 /* Available bits for boost_drv state */
 #define SCREEN_AWAKE		BIT(0)
 #define INPUT_BOOST		BIT(1)
-#define WAKE_BOOST		BIT(2)
-#define MAX_BOOST		BIT(3)
+#define MAX_BOOST		BIT(2)
 
 struct boost_drv {
 	struct workqueue_struct *wq;
@@ -93,7 +92,7 @@ static void unboost_all_cpus(struct boost_drv *b)
 		!cancel_delayed_work_sync(&b->max_unboost))
 		return;
 
-	clear_boost_bit(b, INPUT_BOOST | WAKE_BOOST | MAX_BOOST);
+	clear_boost_bit(b, INPUT_BOOST | MAX_BOOST);
 	update_online_cpu_policy();
 }
 
@@ -136,6 +135,11 @@ void cpu_input_boost_kick_max(unsigned int duration_ms)
 	__cpu_input_boost_kick_max(b, duration_ms);
 }
 
+void cpu_input_boost_kick_wake(void)
+{
+	cpu_input_boost_kick_max(CONFIG_WAKE_BOOST_DURATION_MS);
+}
+
 static void input_boost_worker(struct work_struct *work)
 {
 	struct boost_drv *b = container_of(work, typeof(*b), input_boost);
@@ -176,7 +180,7 @@ static void max_unboost_worker(struct work_struct *work)
 	struct boost_drv *b = container_of(to_delayed_work(work),
 					   typeof(*b), max_unboost);
 
-	clear_boost_bit(b, WAKE_BOOST | MAX_BOOST);
+	clear_boost_bit(b, MAX_BOOST);
 	update_online_cpu_policy();
 }
 
